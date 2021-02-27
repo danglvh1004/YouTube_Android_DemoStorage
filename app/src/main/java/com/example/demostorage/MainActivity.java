7276d2cacbad134bc1656c26e543f08d849e9cf9
danglvh1004 <lvhd.hdlv@gmail.com>
2021-02-27 14:03:11 +0700

    Commit

--- app/src/main/java/com/example/demostorage/MainActivity.java
+++ app/src/main/java/com/example/demostorage/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.demostorage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUsername, edtPassword;
+    private Button btnSaveData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        edtUsername = findViewById(R.id.edt_username);
+        edtPassword = findViewById(R.id.edt_password);
+        btnSaveData = findViewById(R.id.btn_save_data);
+
+        btnSaveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData() {
+
     }
 }
